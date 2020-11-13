734634059ac00f1adbb343d6cdd07db128229a93
smp <2263574372@qq.com>
2020-11-13 19:25:33 +0800

    调整代码

--- export_web_manager/src/test/java/com/smp/web/controller/company/TestTryCatch.java
+++ export_web_manager/src/test/java/com/smp/web/controller/company/TestTryCatch.java
@@ -6,9 +6,9 @@ public class TestTryCatch {
     @Test
     public void test01(){
         try {
-            int num=0;
+            //int num=0;
             System.out.println("Hello");
-            System.out.println(1/num);
+            System.out.println(1/0);
             System.out.println("World");
         } catch (Exception e) {
             e.printStackTrace();
